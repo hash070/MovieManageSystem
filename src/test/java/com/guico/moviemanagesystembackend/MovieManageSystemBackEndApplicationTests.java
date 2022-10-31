eb5654cd52ea95ea6487ee891137d1728a12db28
guico <1305487489@qq.com>
2022-10-31 08:49:02 +0800

    Initial commit

---
+++ src/test/java/com/guico/moviemanagesystembackend/MovieManageSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guico.moviemanagesystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManageSystemBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
