9b3557b03f91b51b125c11495191217a45304bb0
guico <1305487489@qq.com>
2022-10-31 20:53:29 +0800

    验证码功能正常，此部分功能完成

--- src/main/java/com/guico/moviemanagesystembackend/controller/UserController.java
+++ src/main/java/com/guico/moviemanagesystembackend/controller/UserController.java
@@ -5,13 +5,20 @@ import com.guico.moviemanagesystembackend.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/user")
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
     IUserService userService;
+
+    @PostMapping("/code")
+    public Result sendCode(String email){
+        return userService.sendCode(email);
+    }
 
     @PostMapping("/login")
     public Result login(String email, String password) {
