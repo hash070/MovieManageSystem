0863b8512504865997e4fd31dda31db838837991
hash070 <hash070@outlook.com>
2022-10-31 20:11:32 +0800

    初步完成登录界面

---
+++ react-front-end/src/Componments/HelloWorld2.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HelloWorld2 extends Component {
+    render() {
+        return (
+            <div>
+                这里是第二个HelloWorld，您当前正在访问/admin路径
+            </div>
+        );
+    }
+}
+
+export default HelloWorld2;
