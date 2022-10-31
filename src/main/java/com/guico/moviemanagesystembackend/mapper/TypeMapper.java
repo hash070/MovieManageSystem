d85604e61f0c152db2a8768ba3ffd628b8451fb9
guico <1305487489@qq.com>
2022-10-31 17:10:58 +0800

    完成了用户登录验证码接口的编码，开始测试

--- src/main/java/com/guico/moviemanagesystembackend/mapper/TypeMapper.java
+++ src/main/java/com/guico/moviemanagesystembackend/mapper/TypeMapper.java
@@ -2,7 +2,9 @@ package com.guico.moviemanagesystembackend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.guico.moviemanagesystembackend.entry.Type;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface TypeMapper extends BaseMapper<Type> {
 
 }
