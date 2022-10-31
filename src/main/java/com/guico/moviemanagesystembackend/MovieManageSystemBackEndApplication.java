a68dc0308e26852efdf8abeefbc51fa3599314f6
guico <1305487489@qq.com>
2022-10-31 08:49:02 +0800

    Initial commit

---
+++ src/main/java/com/guico/moviemanagesystembackend/MovieManageSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.guico.moviemanagesystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManageSystemBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieManageSystemBackEndApplication.class, args);
+    }
+
+}
