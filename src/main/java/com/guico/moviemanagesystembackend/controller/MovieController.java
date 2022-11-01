4660d409d36e88d2d73eb051dbf3df1870887c6a
guico <1305487489@qq.com>
2022-11-01 17:36:14 +0800

    完成了电影类型的添加服务,并为所有的Controller添加了@CrossOrigin注解

--- src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
+++ src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
@@ -1,7 +1,9 @@
 package com.guico.moviemanagesystembackend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin
 public class MovieController {
 }
