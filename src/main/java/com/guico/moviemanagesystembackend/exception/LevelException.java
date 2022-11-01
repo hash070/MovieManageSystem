0f9663cca9abdf1635c7af0c5ed2c4d34613b200
guico <1305487489@qq.com>
2022-11-01 10:43:40 +0800

    添加了电影类型的权限拦截器和全局异常处理器

---
+++ src/main/java/com/guico/moviemanagesystembackend/exception/LevelException.java
@@ -0,0 +1,7 @@
+package com.guico.moviemanagesystembackend.exception;
+
+public class LevelException extends RuntimeException {
+    public LevelException(String message) {
+        super(message);
+    }
+}
