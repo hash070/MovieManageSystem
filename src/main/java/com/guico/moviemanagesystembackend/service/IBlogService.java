170bc6d76c35f8dd2a75c04baf295c75e10ced8c
guico <1305487489@qq.com>
2022-11-02 22:04:00 +0800

    编码blog接口，准备测试

--- src/main/java/com/guico/moviemanagesystembackend/service/IBlogService.java
+++ src/main/java/com/guico/moviemanagesystembackend/service/IBlogService.java
@@ -2,7 +2,20 @@ package com.guico.moviemanagesystembackend.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.guico.moviemanagesystembackend.entry.Blog;
+import com.guico.moviemanagesystembackend.utils.Result;
 
 public interface IBlogService extends IService<Blog> {
+
+    Result getAll();
+
+    Result getBlogByAuthorId(String authorId);
+
+    Result getBlogBySearch(String search);
+
+    Result addBlog(Blog blog);
+
+    Result deleteBlog(String blogId);
+
+    Result updateBlog(Blog blog);
 
 }
