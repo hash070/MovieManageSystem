f0231a24f8c59cb3cb056b5ac5ad5dcd4ba4feef
hash070 <hash070@outlook.com>
2022-11-04 15:14:40 +0800

    更新后端端口

--- react-front-end/vite.config.js
+++ react-front-end/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8901",
+        target: "http://127.0.0.1:28545",
         changeOrigin: true,
 //                rewrite: (path) => path.replace(/^\/api/, '')
       }
