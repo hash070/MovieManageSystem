b1b6700a7e01a544a45e921bb523e5db7e287239
hash070 <hash070@outlook.com>
2022-11-04 14:12:32 +0800

    修复管理后台菜单偶尔加载失败的问题

--- react-front-end/src/Componments/HelloWorld1.jsx
+++ react-front-end/src/Componments/HelloWorld1.jsx
@@ -8,6 +8,7 @@ class HelloWorld1 extends Component {
                 这里是第一个HelloWorld，您当前正在访问根路径
                 //使用Navigate组件进行重定向
                 <br/>
+                <video src={'https://ts.hash070.top/103/Haganma.mp4?hash=AgADdA'} controls={true} width={500} height={300}/>
                 <NavLink to={'/admin'} >跳转到管理后台</NavLink>
             </div>
         );
