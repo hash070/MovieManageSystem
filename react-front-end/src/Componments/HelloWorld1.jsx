fd5d00ddf74f25e0dfad844aae87a9de83071b2d
hash070 <hash070@outlook.com>
2022-11-04 22:50:21 +0800

    自托管Runner测试

--- react-front-end/src/Componments/HelloWorld1.jsx
+++ react-front-end/src/Componments/HelloWorld1.jsx
@@ -7,12 +7,11 @@ function HelloWorld1(props) {
     const navigate = useNavigate()
     return (
         <div>
-            这里是第一个HelloWorld，您当前正在访问根路径
-            //使用Navigate组件进行重定向
-            <br/>
-            <video src={'https://ts.hash070.top/103/Haganma.mp4?hash=AgADdA'} controls={true} width={500} height={300}/>
+            您当前正在访问根路径
             <br/>
             <Button type={"primary"} onClick={() => navigate('/admin')}>跳转到admin</Button>
+            只因哥视频播放测试
+            <video src={'https://ts.hash070.top/103/Haganma.mp4?hash=AgADdA'} controls={true} width={500} height={300}/>
         </div>
     );
 }
