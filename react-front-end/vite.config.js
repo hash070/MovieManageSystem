4c43910c1e9350f7285e4a3f2e6842a0b13db7e0
hash070 <hash070@outlook.com>
2022-11-04 17:11:40 +0800

    添加React前端CI/CD

--- react-front-end/vite.config.js
+++ react-front-end/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:28545",
+        // target: "http://127.0.0.1:28545",
+        target: "https://movie.hash070.top",
         changeOrigin: true,
 //                rewrite: (path) => path.replace(/^\/api/, '')
       }
