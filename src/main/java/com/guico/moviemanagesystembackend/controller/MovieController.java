07fdbd8ba6ac97594ecf6f619de4df7e758c7e26
guico <83762929+guico52@users.noreply.github.com>
2022-11-08 19:06:57 +0800

    修复下载文件的路径问题 (#35)
    * 修改电影服务接口
    
    * 修正电影更新结构，并添加文件检查
    
    * 修正下载文件时无法找到文件的问题
    
    * 修正下载文件时无法找到文件的问题

--- src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
+++ src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
@@ -64,7 +64,7 @@ public class MovieController {
 
     @PostMapping("/update")
     public Result updateMovieMsg(Long id, String name, String des, Integer typeId, String tags,
-                                 Boolean visibility, MultipartFile pic) throws IOException {
+                                 Boolean visibility, String pic) throws IOException {
         return movieService.updateMovieMsg(id, name, des, typeId, tags, visibility, pic);
     }
 
@@ -80,9 +80,10 @@ public class MovieController {
 
     @RequestMapping("/getFile")
     public Result getFile(String url){
+        url = path + url;
         File file = new File(url);
         if(file.exists()){
-            response.setHeader("Content-Disposition", "attachment;filename="+ path + file.getName());
+            response.setHeader("Content-Disposition", "attachment;filename="+ url);
             response.setContentType("application/octet-stream");
             response.setHeader("Content-Length", String.valueOf(file.length()));
             byte[] buffer = new byte[(int)file.length()];
