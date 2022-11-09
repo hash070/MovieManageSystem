7a8e04c450433d0e3b6d980a55c9faec5f5fb73c
guico <1305487489@qq.com>
2022-11-09 11:53:46 +0800

    修复电影无法更新的问题

--- src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
+++ src/main/java/com/guico/moviemanagesystembackend/controller/MovieController.java
@@ -17,8 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin
