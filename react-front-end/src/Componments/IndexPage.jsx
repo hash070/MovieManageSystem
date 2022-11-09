49d8b7a428d9089c32782d70db8d5b9ab50a85c3
hash070 <hash070@outlook.com>
2022-11-09 15:59:16 +0800

    feat: 完成网站文章列表加载功能

--- react-front-end/src/Componments/IndexPage.jsx
+++ react-front-end/src/Componments/IndexPage.jsx
@@ -1,6 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import {NavLink, useNavigate} from "react-router-dom";
-import {Button, List} from "antd";
+import {Avatar, Button, Divider, List, Skeleton} from "antd";
 import '../styles/HomePage.css'
 import axios from "axios";
 import {convertTypeObjToAntDList, errorMSG} from "../Utils/CommonFuncs.js";
@@ -10,18 +10,26 @@ function IndexPage(props) {
     //获取路由跳转方法
     const navigate = useNavigate()
 
-    //列表数据
-    const [list_data, setListData] = useState([{
+    //分类列表数据
+    const [type_list_data, setTypeListData] = useState([{
         title:
             '暂无分类'
     }])
 
+    //文章列表数据
+    const [blog_list_data,setBlogListData] = useState([])
+    //视频列表数据
+    const [video_list_data,setVideoListData] = useState([])
+
+    //列表加载动画状态
+    const [initLoading, setInitLoading] = useState(true);
+    //绑定Hooks函数，控制列表是否重新加载
     const [loading, setLoading] = useState(false)
 
     //分类按钮点击事件处理
 
     const onTypeBtnClicked = (item) => {
-        console.log('点击的分类ID为：',item.id)
+        console.log('点击的分类ID为：', item.id)
     }
 
     //网页HeaderJS加载Hooks
@@ -78,10 +86,9 @@ function IndexPage(props) {
         });
     })
 
-    //获取分裂列表Hooks
+    //获取分类、视频与文章数据列表Hooks函数
     useEffect(() => {
-        console.log('开始获取所有分类')
-        //发送请求
+        console.log('开始获取分类列表')
         axios.post('/api/type/getAll')
             .then((res) => {
                 console.log('返回结果', res.data)
@@ -93,13 +100,36 @@ function IndexPage(props) {
                 //转换数据，适配AntD List
                 let data_recv = convertTypeObjToAntDList(res.data.data)
                 //设置数据
-                setListData(data_recv)
+                setTypeListData(data_recv)
+            })
+        console.log('开始获取文章列表')
+        //发送请求
+        axios.post('/api/blog/getAll')
+            .then((res) => {
+                console.log('返回结果', res.data)
+                if (!res.data.success) {//检查是否成功
+                    //如果失败，则做出提示，然后直接返回
+                    errorMSG('获取分类列表失败：' + res.data.errorMsg)
+                    setInitLoading(false)
+                    setBlogListData([])
+                    return
+                }
+                let data_recv = res.data.data
+                //设置文章列表数据
+                setBlogListData(data_recv)
+            })
+            .catch((err) => {
+                console.log('错误信息', err)
+                errorMSG(err.message + '请检查网络连接')
+            })
+            .finally(() => {
+                //关闭加载状态
+                setInitLoading(false)
             })
     }, [loading])
 
     return (
         <div>
-
             <header>
                 <div><img src="/imgs/1.png"/></div>
                 <div><img src="/imgs/2.png"/></div>
@@ -118,9 +148,9 @@ function IndexPage(props) {
                     type={'primary'}
                 >管理后台</Button>
             </header>
-            <br/>
-            <br/>
             <div>
+                <Divider>影片分类</Divider>
+                {/*电影分裂列表*/}
                 <List
                     grid={{
                         gutter: 0,
@@ -132,20 +162,62 @@ function IndexPage(props) {
                         xxl: 13
                     }}
                     style={{marginLeft: '40px'}}
-                    dataSource={list_data}
+                    dataSource={type_list_data}
                     renderItem={(item) => (
                         <List.Item>
                             <Button
                                 style={{
                                     backgroundColor: "#f4f5f6",
                                     borderRadius: "10px",
+                                    borderColor: '#fff'
                                 }}
                                 type="default"
                                 shape="default"
-                                onClick={()=>onTypeBtnClicked(item)}
+                                onClick={() => onTypeBtnClicked(item)}
                             >
                                 {item.title}
                             </Button>
+                        </List.Item>
+                    )}
+                />
+                <Divider>网站视频</Divider>
+                <List
+                    loading={initLoading}
+                    itemLayout="horizontal"
+                    dataSource={video_list_data}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Skeleton avatar title={false} loading={item.loading} active>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={'https://img.hash070.top/i/63677e3963348.webp'}/>}
+                                    title={<a style={{maxWidth: '90%', wordBreak: 'break-all'}}>{item.title}</a>}
+                                    description={<div style={{maxWidth: '90%', wordBreak: 'break-all'}}>{item.des}</div>}
+                                />
+                                <div>作者：{item.author}</div>
+                            </Skeleton>
+                        </List.Item>
+                    )}
+                />
+                <Divider>网站文章</Divider>
+                {/*文章列表*/}
+                <List
+                    loading={initLoading}
+                    itemLayout="horizontal"
+                    dataSource={blog_list_data}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Skeleton avatar title={false} loading={item.loading} active>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={'https://img.hash070.top/i/63677e3963348.webp'}/>}
+                                    title={<a style={{maxWidth: '70%', wordBreak: 'break-all'}}>{item.title}</a>}
+                                    description={<div style={{maxWidth: '70%', wordBreak: 'break-all'}}>{item.des}</div>}
+                                />
+                                <div style={{
+                                    position: "absolute",
+                                    right: '0px',
+                                    marginRight: '50px'
+                                }}>作者：{item.author}</div>
+                            </Skeleton>
                         </List.Item>
                     )}
                 />
