5f71b2458a9a4df456cb15cecbb25f31730bce73
hash070 <hash070@outlook.com>
2022-11-09 20:41:34 +0800

    refactor: 提取前台头部动画JS函数到工具类

--- react-front-end/src/Componments/Front/MovieIndex.jsx
+++ react-front-end/src/Componments/Front/MovieIndex.jsx
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Outlet, useNavigate} from "react-router-dom";
 import {Button} from "antd";
 import {ArrowLeftOutlined} from "@ant-design/icons";
+import {headerJSMovement} from "../../Utils/CommonFuncs.js";
+import '../../styles/HomePage.css'
 
 function MovieIndex(props) {
     const navigate = useNavigate()
+
+    //网页头部JS动态效果加载Hooks
+    useEffect(() => {
+        headerJSMovement()
+    },[])
     return (
         <div>
-            <Button
-                className='radius-btn'
-                style={{
-                    position: "absolute",
-                    left: '0px',
-                    top: '0px',
-                    margin: '20px',
-                }}
-                icon={<ArrowLeftOutlined/>}
-                type={'primary'}
-                onClick={() => {
-                    navigate('/')
-                }}
-            >返回首页</Button>
-            <p>这里是电影根目录</p>
+            <header>
+                <div><img src="/imgs/1.png"/></div>
+                <div><img src="/imgs/2.png"/></div>
+                <div><img src="/imgs/3.png"/></div>
+                <div><img src="/imgs/4.png"/></div>
+                <div><img src="/imgs/5.png"/></div>
+                <div><img src="/imgs/6.png"/></div>
+                <Button
+                    className='radius-btn'
+                    style={{
+                        position: "absolute",
+                        left: '0px',
+                        top: '0px',
+                        margin: '20px',
+                    }}
+                    icon={<ArrowLeftOutlined/>}
+                    type={'primary'}
+                    onClick={() => {
+                        navigate('/')
+                    }}
+                >返回首页</Button>
+                <Button
+                    className='radius-btn'
+                    style={{
+                        position: "absolute",
+                        right: '0px',
+                        margin: '20px'
+                    }}
+                    onClick={() => navigate('/admin')}
+                    type={'primary'}
+                >管理后台</Button>
+            </header>
+            {/*文章容器*/}
             <div
-                className="site-layout-background"
                 style={{
                     padding: 24,
                     minHeight: 360,
+                    paddingTop: '64px',
+                    maxWidth: '800px',
+                    width: '100%',
+                    left: 0,
+                    right: 0,
+                    margin: 'auto'
                 }}
             >
                 <Outlet/>
--- react-front-end/src/Utils/CommonFuncs.js
+++ react-front-end/src/Utils/CommonFuncs.js
@@ -107,3 +107,57 @@ export function convertTypeObjToAntDList(list) {
     }
     return antdList
 }
+
+// 前台头部JS动态效果
+export function headerJSMovement(){
+    var startX = 0;
+    let blurValue;
+    const images = document.querySelectorAll("header>div>img");
+
+    document.querySelector("header").addEventListener("mousemove", (e) => {
+        let offsetX = e.clientX - startX + 482;
+        let percentage = offsetX / window.outerWidth;
+        let offset = 15 * percentage;
+        let blur = 20;
+
+        for (let [index, image] of images.entries()) {
+            offset *= 1.3;
+            blurValue =
+                Math.pow(index / images.length - percentage, 2) * blur;
+            image.style.setProperty("--offset", `${offset}px`);
+            image.style.setProperty("--blur", `${blurValue}px`);
+        }
+    });
+    document.querySelector("header").addEventListener("mouseover", (e) => {
+        startX = e.clientX;
+        for (let [index, image] of images.entries()) {
+            image.style.transition = "none";
+        }
+    });
+
+    document.querySelector("header").addEventListener("mouseout", () => {
+        let offsetX = 482;
+        let blur = 20;
+        let percentage = offsetX / window.outerWidth;
+        let offset = 15 * percentage;
+        for (let [index, image] of images.entries()) {
+            offset *= 1.3;
+            blurValue = Math.pow(index / images.length - percentage, 2) * blur;
+            image.style.setProperty("--offset", `${offset}px`);
+            image.style.setProperty("--blur", `${blurValue}px`);
+            image.style.transition = "all .3s ease";
+        }
+    });
+    window.addEventListener("load", () => {
+        let offsetX = 482;
+        let blur = 20;
+        let percentage = offsetX / window.outerWidth;
+        let offset = 15 * percentage;
+        for (let [index, image] of images.entries()) {
+            offset *= 1.3;
+            blurValue = Math.pow(index / images.length - percentage, 2) * blur;
+            image.style.setProperty("--offset", `${offset}px`);
+            image.style.setProperty("--blur", `${blurValue}px`);
+        }
+    });
+}
